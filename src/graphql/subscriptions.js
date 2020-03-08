ce4466907fabd0a7e97c083783cd115996aad59d
mike.mo <mike.mo.jianxin@gmail.com>
2020-03-09 10:12:51 +1100

    1. run the amplify add hosting 2. amplify push

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      owner
+      date
+      description
+      completed
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      owner
+      date
+      description
+      completed
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      owner
+      date
+      description
+      completed
+    }
+  }
+`;
